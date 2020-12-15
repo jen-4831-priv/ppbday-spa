e27207b0464625b1577db525f569efaf458ec02f
nzhe4831 <jennnai.rl4831@gmail.com>
2020-12-15 23:40:02 +1100

    Add styles

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,4 +3,5 @@ import React from 'react';
 const Home = () => {
   return <div></div>;
 };
+// eslint-disable-next-line import/no-default-export
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
+import './index.scss';
 
 ReactDOM.render(
   <BrowserRouter>
